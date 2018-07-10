c032a2bcb833c7fa70bc4ea32f3a3c532bfd0caf
Aurelia <aurela87@o2.pl>
2018-07-10 17:49:31 +0200

    create project

---
+++ src/main/java/Rabit.java
@@ -0,0 +1,10 @@
+public class Rabit {
+    int maleCount;
+    int femaleCount;
+    int longLimit;
+
+    int simulate(int maleCount, int femaleCount, int longLimit) {
+        return 0;
+    }
+
+}
